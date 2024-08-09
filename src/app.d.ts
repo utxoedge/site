8f6c323c06c989d248984d926d08022c55798921
rvcas <x@rvcas.dev>
2024-08-08 23:54:56 -0400

    feat: add workspaces query

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,15 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
+		interface Platform {
+			env: {
+				TOKENS: KVNamespace;
+				ACCOUNTS: D1Database;
+			};
+			context: {
+				waitUntil(promise: Promise<unknown>): void;
+			};
+		}
 	}
 }
 
---
+++ src/routes/workspaces/+page.server.ts
@@ -0,0 +1,27 @@
+import { eq } from 'drizzle-orm';
+
+// Lib
+import { drizzle } from '$lib/server/drizzle';
+import * as schema from '$lib/server/schema.sql';
+
+// Locals
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ platform }) => {
+	const db = drizzle(platform!.env.ACCOUNTS);
+
+	const workspaces = await db
+		.select({
+			id: schema.workspaces.id,
+			slug: schema.workspaces.slug,
+			name: schema.workspaces.name,
+			createdAt: schema.workspaces.createdAt,
+			updatedAt: schema.workspaces.updatedAt
+		})
+		.from(schema.workspaces)
+		.innerJoin(schema.users, eq(schema.workspaces.id, schema.users.workspaceId))
+		.where(eq(schema.users.identityId, identity!.id))
+		.execute();
+
+	return { workspaces };
+};
