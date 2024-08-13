57dd5ea95fc185bca46af95ac107f56da313ab5a
rvcas <x@rvcas.dev>
2024-08-13 19:49:00 -0400

    chore: debug some shit

--- src/routes/workspaces/[slug]/+page.server.ts
+++ src/routes/workspaces/[slug]/+page.server.ts
@@ -11,6 +11,8 @@ export const load: PageServerLoad = async ({ url, locals, platform }) => {
 
   const { page, pageSize } = getPaginationParams(url);
 
+  console.log(page, pageSize);
+
   const apiKeys = await db.query.apiKeys
     .findMany({
       where(fields, { and, eq }) {
@@ -24,6 +26,8 @@ export const load: PageServerLoad = async ({ url, locals, platform }) => {
     })
     .execute();
 
+  console.log(apiKeys);
+
   const result = await db
     .select({
       count: count(schema.apiKeys.id),
@@ -36,6 +40,8 @@ export const load: PageServerLoad = async ({ url, locals, platform }) => {
       ),
     )
     .execute();
+
+  console.log(result);
 
   const totalCount = result[0]?.count;
 
